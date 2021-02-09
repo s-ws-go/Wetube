72bf2bd6758a5ab6e016f748d0766803d27851fd
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-02-09 11:43:53 +0900

    #2 babel setting

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const express = require("express");
+import express from "express";
+import "core-js";
 const app = express();
 const PORT = 4000;
 
 const handleListening = () => {
   console.log(`Listening on : http://localhost:${PORT}`);
 };
+
+const handleHome = (req, res) => {
+  res.send("Hello from home??");
+};
+
+const handleProfile = (req, res) => {
+  res.send("This is Your Profile!");
+};
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
