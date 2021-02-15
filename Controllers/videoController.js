e29f407274c3e697b7bb58115456bffb9855b11a
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-02-15 16:24:08 +0900

    #10 Home&Search controller

--- Controllers/videoController.js
+++ Controllers/videoController.js
@@ -1,12 +1,14 @@
-export const home = (req, rep) => rep.render("home", { pageTitle: "HOME" });
+import { videos } from "../db";
+export const home = (req, rep) =>
+  rep.render("home", { pageTitle: "HOME", videos });
 export const search = (req, rep) => {
   const {
     query: { term: searchingBy },
   } = req;
-  rep.render("search", { pageTitle: "SEARCH", searchingBy });
+  rep.render("search", { pageTitle: "SEARCH", searchingBy, videos });
 };
-export const videos = (req, rep) =>
-  rep.render("videos", { pageTitle: "VIDEOS" });
+// export const videos = (req, rep) =>
+//   rep.render("videos", { pageTitle: "VIDEOS" });
 export const upload = (req, rep) =>
   rep.render("upload", { pageTitle: "UPLOAD" });
 export const videoDetail = (req, rep) =>
