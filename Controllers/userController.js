4e025e013032014ac6e47ba086f00bb80bad3e4c
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-02-15 12:13:08 +0900

    7-3 pug 3_localvariables..

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,9 +1,13 @@
-export const join = (req, rep) => rep.render("join");
-export const login = (req, rep) => rep.render("login");
-export const logout = (req, rep) => rep.render("logout");
+export const join = (req, rep) => rep.render("join", { pageTitle: "JOIN" });
+export const login = (req, rep) => rep.render("login", { pageTitle: "LOGIN" });
+export const logout = (req, rep) =>
+  rep.render("logout", { pageTitle: "LOGOUT" });
 export const users = (req, rep) => {
   return rep.render("users");
 };
-export const userDetail = (req, rep) => rep.render("userDetail");
-export const editProfile = (req, rep) => rep.render("editProfile");
-export const changePassword = (req, rep) => rep.render("changePassword");
+export const userDetail = (req, rep) =>
+  rep.render("userDetail", { pageTitle: "DETAIL USER" });
+export const editProfile = (req, rep) =>
+  rep.render("editProfile", { pageTitle: "EDIT PROFILE" });
+export const changePassword = (req, rep) =>
+  rep.render("changePassword", { pageTitle: "CHANGE PASSWORD" });
--- Controllers/videoController.js
+++ Controllers/videoController.js
@@ -1,7 +1,13 @@
-export const home = (req, rep) => rep.render("home");
-export const search = (req, rep) => rep.render("search");
-export const videos = (req, rep) => rep.render("videos");
-export const upload = (req, rep) => rep.render("upload");
-export const videoDetail = (req, rep) => rep.render("videoDetail");
-export const editVideo = (req, rep) => rep.render("editVideo");
-export const deleteVideo = (req, rep) => rep.render("deleteVideo");
+export const home = (req, rep) => rep.render("home", { pageTitle: "HOME" });
+export const search = (req, rep) =>
+  rep.render("search", { pageTitle: "SEARCH" });
+export const videos = (req, rep) =>
+  rep.render("videos", { pageTitle: "VIDEOS" });
+export const upload = (req, rep) =>
+  rep.render("upload", { pageTitle: "UPLOAD" });
+export const videoDetail = (req, rep) =>
+  rep.render("videoDetail", { pageTitle: "DETAIL VIDEO" });
+export const editVideo = (req, rep) =>
+  rep.render("editVideo", { pageTitle: "EDIT VIDEO" });
+export const deleteVideo = (req, rep) =>
+  rep.render("deleteVideo", { pageTitle: "DELETE VIDEO" });
