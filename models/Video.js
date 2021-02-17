dcc79419eb47a0ca2a7b54841fd08b01c5494110
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-02-17 11:03:22 +0900

    #13-2 video model

---
+++ models/Video.js
@@ -0,0 +1,27 @@
+import mongoose, { Mongoose } from "mongoose";
+
+//비디오를 db에 넣는게 아니라 링크 따오는 거임.
+const VideoSchema = new mongoose.Schema({
+  fileurl: {
+    type: String,
+    required: "File URL is required",
+  },
+  title: {
+    type: String,
+    required: "Title is required",
+  },
+  description: String,
+  views: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//schema를 이용해서 model 만들기
+
+const model = mongoose.model("Video", VideoSchema);
+export default model;
