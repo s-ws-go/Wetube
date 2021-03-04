7e21260b889ea3612714b85cbec9fc9fa822dba0
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-03-04 19:42:51 +0900

    22-4 comments fin(fake adding)

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,5 +1,21 @@
 import axios from "axios";
 const addCommentForm = document.getElementById("jsAddComment");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
+
+const increaseNumber = () => {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+};
+
+const fakeComment = (comment) => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  commentList.prepend(li);
+  //prepend(a) : a를 앞에다 추가해 줌
+  increaseNumber();
+};
 
 const sendComment = async (comment) => {
   const videoID = window.location.href.split("/videos/")[1];
@@ -11,6 +27,9 @@ const sendComment = async (comment) => {
     },
     //comment : comment 임.(videoController에서 비구조화 할당했던 comment)
   });
+  if (response.status === 200) {
+    fakeComment(comment);
+  }
   console.log(response);
 };
 
