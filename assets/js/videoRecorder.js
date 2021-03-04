2cd5b869ab1bf71de985b737b68a8f0dc0d054c4
s-ws-go <75929950+s-ws-go@users.noreply.github.com>
2021-03-04 15:49:00 +0900

    #21-2 recording 2

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,25 +2,42 @@ const recordContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+let streamObject;
+
+const handleVideoData = (e) => {
+  console.log(e);
+};
+
+const startRecording = () => {
+  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  setTimeout(() => videoRecorder.stop(), 5000);
+  //MediaRecorder 특성상, 레코딩이 다 끝나야 handleVideoData가 호출됨
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: true,
     });
-    console.log(stream);
     videoPreview.srcObject = stream;
+    videoPreview.muted = true;
     videoPreview.play();
     recordBtn.innerHTML = "Stop recording!";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
     console.log(error);
     recordBtn.innerHTML = "T_T Cannot record!";
-    recordBtn.removeEventListener("click", startRecording);
-  }
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
+  } // try든 catch든 이걸로 끝남
 };
 
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recordContainer) {
